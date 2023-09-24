0a4d0e509c4525cb242bf19757d05ccfcffa4733
Andrew Aguh <andrew.aguh@gmail.com>
2023-09-24 20:56:50 +0100

    Create general layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ScreenComponent } from './screen/screen.component';
+import { CurrencySelectorComponent } from './currency-selector/currency-selector.component';
+import { AmountComponent } from './amount/amount.component';
+import { ExchangeRateComponent } from './exchange-rate/exchange-rate.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ScreenComponent,
+    CurrencySelectorComponent,
+    AmountComponent,
+    ExchangeRateComponent
   ],
   imports: [
     BrowserModule,
