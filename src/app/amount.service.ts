4368c73aee1f90249bf21a58a96fd2fc8ada16e8
Andrew Aguh <andrew.aguh@gmail.com>
2023-10-01 17:00:09 +0100

    string populated with each number pressed

--- src/app/amount.service.ts
+++ src/app/amount.service.ts
@@ -5,12 +5,21 @@ import { Injectable } from '@angular/core';
 })
 export class AmountService {
 
-  constructor() { }
+  x = "" 
+  constructor() { 
+  }
+
+  
 
   // Updates value of clicked variable upon each 
   update(character: KeyboardEvent){
-    console.log(character)
     // WIP - should run this method each time GBP value changed
+    console.log(character)
+
+    if (character.code.substring(0,5) === "Digit"){
+      this.x += character.key;
+      console.log(this.x);
+    }
 
     if (character.key == "Backspace"){
       console.log("backspace clicked")
