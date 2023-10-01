879eb3bf59a2dacfd69eebe5739fa47f5dccdc0a
Andrew Aguh <andrew.aguh@gmail.com>
2023-10-01 13:06:01 +0100

    add functional API call

---
+++ src/app/currency-api-service.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CurrencyAPIServiceService {
+
+  constructor(private http:HttpClient) { }
+
+  getData(){
+    let url = "http://data.fixer.io/api/latest?access_key=82bd88359fb77e1b53f0c6f2f38f4a5a&format=1" /* API URL*/
+    return this.http.get(url); //get() returns an observable representing the HTTP response that will be received from the specified URL
+  }
+}
--- src/app/currency-selector/currency-selector.component.ts
+++ src/app/currency-selector/currency-selector.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { CurrencyAPIServiceService } from '../currency-api-service.service';
 
 @Component({
   selector: 'app-currency-selector',
@@ -7,11 +8,14 @@ import { HttpClient } from '@angular/common/http';
   styleUrls: ['./currency-selector.component.css']
 })
 export class CurrencySelectorComponent implements OnInit{
-  constructor(private http: HttpClient){
+  constructor(private http: HttpClient, private api: CurrencyAPIServiceService){
 
   }
 
   ngOnInit(): void {
+    this.api.getData().subscribe((response) => {
+      console.log(response)
+    })
       
   }
 
